b397f4f18d19a58d7b68426e94c96c606ac7fd87
Alejandro Rubio <darubiio97@icloud.com>
2025-05-25 12:04:29 +0200

    feat: :construction: add bar chart with item stock details

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,20 @@
 import { FC } from "react";
-import { getItemDetails } from "../../lib/zohoData";
+
+import ChartBar from "../../ui/dashboard/ChartBar";
+import { getCategories, getWarehouseCategoryStock } from "../../lib/zohoData";
 
 const DashboardPage: FC = async () => {
-  const itemDetails = await getItemDetails();
+  const dataset = await getCategories()
+  const itemDetails = await getWarehouseCategoryStock();
+
   return (
-    <div></div>
+    <div className="flex flex-col gap-4 p-4">      
+      <div className="card bg-base-200 w-full shadow-sm">
+        <div className="card-body">
+          <ChartBar data={itemDetails} dataset={dataset} />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- lib/zohoData.ts
+++ lib/zohoData.ts
@@ -1,6 +1,15 @@
 import { getAuthToken } from "./zohoAuth";
-import { getWarehousesByLocations } from "./zohoDataUtils";
-import { Item, ItemDetails, Warehouse } from "../types";
+import { getItemCategories, getWarehousesByLocations } from "./zohoDataUtils";
+import {
+  Item,
+  ItemCategories,
+  ItemDetails,
+  Warehouse,
+  WarehouseCategory,
+} from "../types";
+import { Redis } from "@upstash/redis";
+
+const REDIS = Redis.fromEnv();
 
 const { ZOHO_ORG_ID, ZOHO_DOMAIN } = process.env;
 
@@ -35,6 +44,9 @@ export const getWarehouses = async (
   try {
     const token = accessToken || (await getAuthToken());
 
+    const warehousesCache: Warehouse[] | null = await REDIS.get("warehouses");
+    if (warehousesCache) return warehousesCache;
+
     const url = `https://www.zohoapis.${ZOHO_DOMAIN}/inventory/v1/locations?organization_id=${ZOHO_ORG_ID}`;
     const res = await fetch(url, {
       method: "GET",
@@ -47,7 +59,11 @@ export const getWarehouses = async (
       throw new Error("Failed to fetch warehouses");
     }
 
-    if (data.locations) return getWarehousesByLocations(data.locations);
+    if (data.locations) {
+      const warehousesByLocation = getWarehousesByLocations(data.locations);
+      REDIS.set("warehouses", warehousesByLocation, { ex: 5400 });
+      return warehousesByLocation;
+    }
 
     throw new Error("No warehouses found in response");
   } catch (error) {
@@ -81,10 +97,10 @@ const getItems = async (accessToken?: string): Promise<Item[]> => {
   }
 };
 
-const getItemsDetailsByItemsId = async (
+const getItemsDetailByItemsId = async (
   itemIdList: string[],
   accessToken?: string
-) => {
+): Promise<ItemDetails> => {
   try {
     const token = accessToken || (await getAuthToken());
 
@@ -110,21 +126,21 @@ const getItemsDetailsByItemsId = async (
   }
 };
 
-const getSlicedItemsDetailsByItemsId = async (
+const getItemsDetail = async (
   itemIdList: string[],
   accessToken?: string,
-  chunkSize = 30
-): Promise<PromiseSettledResult<ItemDetails>[]> => {
+  chunkSize = 40
+) => {
   if (!itemIdList.length) return [];
   const token = accessToken || (await getAuthToken());
   const chunks = [];
   for (let i = 0; i < itemIdList.length; i += chunkSize) {
     chunks.push(itemIdList.slice(i, i + chunkSize));
   }
-  const results = await Promise.allSettled(
+  const results = await Promise.all(
     chunks.map(async (chunk) => {
       try {
-        return await getItemsDetailsByItemsId(chunk, token);
+        return await getItemsDetailByItemsId(chunk, token);
       } catch (e) {
         console.error("Error fetching chunk", chunk, e);
         return [];
@@ -134,18 +150,93 @@ const getSlicedItemsDetailsByItemsId = async (
   return results.flat();
 };
 
-export const getItemDetails = async (accessToken?: string) => {
+export const getCategories = async () => {
+  try {
+    const items = await getItems();
+    return Array.from(
+      new Map(
+        items
+          .filter(
+            ({ category_id, category_name }) =>
+              Boolean(category_id && category_id.trim()) &&
+              Boolean(category_name && category_name.trim())
+          )
+          .map(({ category_id, category_name }) => [
+            category_id,
+            { category_id, category_name },
+          ])
+      ).values()
+    );
+  } catch (e) {
+    console.error("Error fetching categories", e);
+    return [];
+  }
+};
+
+const getItemDetails = async (accessToken?: string) => {
   try {
     const token = accessToken || (await getAuthToken());
+
+    const itemDetailsCache: ItemDetails[] | null = await REDIS.get(
+      "itemsDetail"
+    );
+
+    if (itemDetailsCache) return itemDetailsCache;
+
     const itemList = await getItems(token);
-    const itemIdList = itemList.map((item) => item.item_id);
-    const itemDetails = await getSlicedItemsDetailsByItemsId(itemIdList, token);
+    const itemCategories = getItemCategories(itemList);
+    const itemIdList = Object.keys(itemCategories);
+    const itemDetails = await getItemsDetail(itemIdList);
 
-    if (itemDetails) return itemDetails;
+    const itemDetailsCategories = itemDetails.map((item) => ({
+      ...item,
+      ...itemCategories[item["item_id"]],
+    }));
 
-    throw new Error("No items found in response");
+    const stringDetails = JSON.stringify(itemDetailsCategories);
+    await REDIS.set("itemsDetail", stringDetails, { ex: 2400 });
+    return itemDetailsCategories;
   } catch (error) {
     console.error("Error fetching token from Redis:", error);
     throw new Error("Failed to fetch token from Redis");
   }
+};
+
+export const getItemsCategoriesStock = async () => {
+  const itemDetails = await getItemDetails();
+  const data: ItemCategories = {};
+
+  for (const item of itemDetails) {
+    const { category_id, warehouses } = item;
+
+    for (const warehouse of warehouses) {
+      const { warehouse_name, warehouse_stock_on_hand } = warehouse;
+      if (!data[category_id]) data[category_id] = { name: category_id };
+      const warehouseName = warehouse_name.replace(/ /g, "_");
+      data[category_id][warehouseName] =
+        data[category_id][warehouseName] ?? 0 + Number(warehouse_stock_on_hand);
+    }
+  }
+
+  return Object.values(data);
+};
+
+export const getWarehouseCategoryStock = async () => {
+  const itemDetails = await getItemDetails();
+  const data: WarehouseCategory = {};
+
+  for (const item of itemDetails) {
+    const { category_id, warehouses } = item;
+
+    for (const warehouse of warehouses) {
+      const { warehouse_name, warehouse_stock_on_hand } = warehouse;
+      if (!data[warehouse_name]) {
+        data[warehouse_name] = { name: warehouse_name };
+      }
+      data[warehouse_name][category_id] =
+        (data[warehouse_name][category_id] ?? 0) + warehouse_stock_on_hand;
+    }
+  }
+
+  return Object.values(data);
 };
